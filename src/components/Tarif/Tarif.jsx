0b7fcc5932f9f5aa46f0d34ab3d2b5beed359534
jellyjul <100496@mail.ru>
2023-06-21 00:57:00 +0500

    added state

--- src/components/Tarif/Tarif.jsx
+++ src/components/Tarif/Tarif.jsx
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Tarif.module.scss'
 import classNames from 'classnames';
 
-
-
-
+// выделяются все тарифы, как сделать чтобы выделялся только один?
+// colorOne and colorTwo оба не работают, в консоли отрисовывается значение селекторов, но не могу понять почему не работает
 class Tarif extends React.Component {
+    constructor(props) {
+                super(props);
+                this.state = {
+                    pressed:false
+                }
+            }
+    
+        handleChange = () => {
+            this.setState(
+                {pressed: !this.state.pressed})
+        }
     render(){
-        const {name, speed,colorOne,colorTwo, isSelected}= this.props;
-        const selected = classNames([styles.tarifCard, isSelected? ` ${styles.selected}`:'']);
+        const {name, speed,colorOne,colorTwo}= this.props;
+        const selected = classNames([styles.tarifCard, this.state.pressed ?  ` ${styles.selected}`:'']);
         const firstColor = classNames([`${colorOne}`, styles.tarifName]);
         const secondColor = classNames([`${colorTwo}`, styles.tarifMain ])
         return (
             
-    <div className={selected} >
+    <div onClick={this.handleChange} className={selected} >
             <div className={firstColor}> Безлимитный {name}</div> 
             <div className={secondColor}  >руб: 
                 <span className={styles.tarifSubtitle}> {name}</span>  /мес
@@ -27,4 +37,36 @@ class Tarif extends React.Component {
 export default Tarif;
 
 
-// //colorOne and colorTwo оба не работают, в консоли отрисовывается значение селекторов, но не могу понять почему не работает
+
+
+
+//  если писать на функциональном компоненте, не могу найти ошибку
+
+
+// export default function Tarif(name, speed,colorOne,colorTwo) {
+
+
+//     const [clicked, setClicked] = useState(true);
+//     const handleChange = () => {
+//         setClicked(!clicked);
+//     };
+//     const selected = classNames([styles.tarifCard, clicked ? ` ${styles.selected}`:'']);
+//     const firstColor = classNames([`${colorOne}`, styles.tarifName]);
+//     const secondColor = classNames([`${colorTwo}`, styles.tarifMain ])
+    
+// return (
+            
+//     <div onClick={handleChange} className={selected} >
+//             <div className={firstColor}> Безлимитный {name}</div> 
+//             <div className={secondColor}  >руб: 
+//                 <span className={styles.tarifSubtitle}> {name}</span>  /мес
+//             </div>
+//             <div className={styles.tarifSpeed}>До {speed} Мб/сек
+//             </div>
+//             <div className={styles.tarifVolume}>  Объем включенного трафика не ограничен
+//             </div>
+//     </div>)}
+//     ;
+
+
+
